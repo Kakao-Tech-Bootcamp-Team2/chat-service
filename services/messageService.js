53833e714c09ce767fe32411c5a71a208580bd2c
Kyumin-Chang <kyumin0529@naver.com>
2024-12-13 08:02:57 +0900

    hi

--- services/messageService.js
+++ services/messageService.js
@@ -17,16 +17,14 @@ class MessageService {
         console.log("[MessageService] Created new user:", user);
       }
 
-      const message = new Message(
-        JSON.stringify({
-          room: data.room,
-          content: data.content,
-          sender: data.sender,
-          type: data.type,
-          mentions: data.mentions,
-          file: data.file,
-        })
-      );
+      const message = new Message({
+        room: data.room,
+        content: data.content,
+        sender: data.sender,
+        type: data.type,
+        mentions: data.mentions,
+        file: JSON.stringify(data.file),
+      });
 
       await message.save();
 
